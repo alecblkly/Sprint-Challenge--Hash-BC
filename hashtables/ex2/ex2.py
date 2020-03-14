e59157363b48b4014873bf596db802a781807a75
Alec Blakeley <alecblkly@gmail.com>
2020-03-14 15:55:37 -0400

    Added notes to ex2 for what needs to be completed for the problem.

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -1,9 +1,6 @@
-#  Hint:  You may not need all of these.  Remove the unused functions.
 from hashtables import (HashTable,
                         hash_table_insert,
-                        hash_table_remove,
-                        hash_table_retrieve,
-                        hash_table_resize)
+                        hash_table_retrieve)
 
 
 class Ticket:
@@ -16,8 +13,19 @@ def reconstruct_trip(tickets, length):
     hashtable = HashTable(length)
     route = [None] * length
 
-    """
-    YOUR CODE HERE
-    """
+    # Starting trip, will have the source of NONE
+    # The final destination, will have the destination of NONE
+    # Doing this, will have our first and last routes for the trip
+    # In between the first and last routes, we will want to link source
+    # and destinations together to create the proper list.
+    # Looping over the list, to match source & destination
+    # key/value pair
+    # Things we have are tickets, length of list, source, and destination
+    # With what we have within the hashtables file, we need to be able to
+    # insert items within the list and we need to be able to retrieve items
+    # within the list. From information given, it does not appear that
+    # we would need to remove items from the list or resize the list
+    # Insert is looking for hash_table, key, and value
+    # Retrieve is looking hash_table and key
 
     pass
